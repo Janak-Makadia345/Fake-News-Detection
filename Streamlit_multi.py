5dfda95f3300b4978a7e8fe3b53c2f8de44be6ff
VikenHadavani <161743944+VikenHadavani@users.noreply.github.com>
2024-03-26 23:40:11 +0530

    Update Streamlit_multi.py

--- Streamlit_multi.py
+++ Streamlit_multi.py
@@ -11,6 +11,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+import string
+
 
 # Load the dataset
 @st.cache_resource
